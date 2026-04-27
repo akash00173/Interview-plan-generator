8aadfd006fcb7bfa9866136a388c2bf5dccaca94
Akash Poddar <akashpoddar311@gmail.com>
2026-04-27 13:37:15 +0530

    Fix CORS for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: function(origin, callback) {
+    return callback(null, true)
+  },
   credentials: true
 }))
 
