21add3b1c48c9e79ad01dd8cb529ecd0d96088a6
Akash Poddar <akashpoddar311@gmail.com>
2026-04-27 13:40:09 +0530

    Debug API URL and fix CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,9 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-  origin: function(origin, callback) {
-    return callback(null, true)
-  },
+  origin: ['https://interviewgeneratorfrontend.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }))
 
